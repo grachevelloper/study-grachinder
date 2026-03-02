f7d2201a4914746c614c60265b64a9a693f3e435
grachevelloper <harizma.yt.2006@gmail.com>
2026-03-02 20:51:26 +0300

    initial setup

---
+++ eslint.config.js
@@ -0,0 +1,163 @@
+import js from '@eslint/js';
+import i18nextPlugin from 'eslint-plugin-i18next';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import reactPlugin from 'eslint-plugin-react';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import {defineConfig} from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+const reactHooks = require('eslint-plugin-react-hooks');
+
+const i18next = i18nextPlugin as any;
+
+export default defineConfig([
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        plugins: {
+            js,
+            'simple-import-sort': simpleImportSort,
+            import: importPlugin,
+            prettier: prettierPlugin,
+            'react-hooks': reactHooks,
+            react: reactPlugin,
+            i18next,
+        },
+        extends: [js.configs.recommended],
+        settings: {
+            react: {
+                version: 'detect',
+            },
+            'import/resolver': {
+                alias: {
+                    map: [
+                        ['@/shared/*', './src/shared/*'],
+                        ['@/typings/*', './src/typings/*'],
+                        ['@/test/*', './src/__test__/*'],
+                        ['@/utils/*', './src/utils/*'],
+                        ['@/locales/*', './public/locales/*'],
+                        ['@/users/*', './src/units/users/*'],
+                        ['@/todos/*', './src/units/todos'],
+                        ['@/articles/*', './src/units/articles'],
+                        ['@/public/*', './public'],
+                    ],
+                    extensions: [
+                        '.ts',
+                        '.tsx',
+                        '.js',
+                        '.jsx',
+                        'json',
+                        'png',
+                        'mp4',
+                        'jpg',
+                    ],
+                },
+            },
+        },
+        languageOptions: {
+            globals: {...globals.browser, ...globals.node, ...globals.jest},
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    },
+    tseslint.configs.recommendedTypeChecked,
+    {
+        languageOptions: {
+            parserOptions: {
+                projectService: true,
+            },
+        },
+    },
+    reactPlugin.configs.flat.recommended,
+    {
+        rules: {
+            '@typescript-eslint/no-unsafe-call': 'off',
+            'no-case-declarations': 'off',
+            '@typescript-eslint/only-throw-error': 'off',
+            '@typescript-eslint/no-unused-vars': 'warn',
+            'react/display-name': 'off',
+            'react/jsx-key': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-member-access': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-floating-promises': 'off',
+            '@typescript-eslint/no-unsafe-argument': 'off',
+            '@typescript-eslint/no-unsafe-return': 'off',
+            'prefer-const': 'error',
+            'no-console': 'warn',
+            'simple-import-sort/imports': 'off',
+            'simple-import-sort/exports': 'off',
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        'parent',
+                        'sibling',
+                        'index',
+                        'object',
+                    ],
+                    pathGroups: [
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                            position: 'before',
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ['internal'],
+                    'newlines-between': 'always',
+                    alphabetize: {
+                        order: 'asc',
+                        caseInsensitive: true,
+                    },
+                },
+            ],
+        },
+    },
+    {
+        files: ['**/*.{jsx,tsx}'],
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react/jsx-uses-react': 'off',
+            'react/jsx-key': 'error',
+            'react/jsx-no-duplicate-props': 'error',
+            'object-curly-spacing': ['warn', 'never'],
+        },
+    },
+    {
+        files: ['**/webpack.config.js', '**/*.config.*', '**/scripts/**'],
+        rules: {
+            'no-console': 'off',
+            '@typescript-eslint/no-var-requires': 'off',
+        },
+    },
+    {
+        files: ['**/*.test.{js,jsx,ts,tsx}', '**/__tests__/**'],
+        rules: {
+            'no-console': 'off',
+        },
+    },
+    {
+        files: ['**/locales/*.json'],
+        rules: {
+            'i18n-json/valid-message-syntax': 'error',
+            'i18n-json/valid-json': 'error',
+        },
+    },
+    {
+        files: ['**/__test__/mocks/**', '**/__test__/utils/**'],
+        rules: {
+            '@typescript-eslint/no-var-requires': 'off',
+        },
+    },
+]);
